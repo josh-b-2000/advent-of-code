597e4f7a76de5a487a5c97ab365c465f59061dd9
josh-b-2000 <joshua@autone.io>
2025-12-01 21:45:08 +0000

    day 1 part 2

--- 2025/day1.zig
+++ 2025/day1.zig
@@ -6,26 +6,41 @@ const Direction = enum { right, left };
 pub fn main() !void {
     var lines = std.mem.tokenizeSequence(u8, data, "\n");
 
-    var dialPosition: i64 = 50;
-    var dialZeroCount: u64 = 0;
+    var file = try std.fs.cwd().createFile("day1_output.txt", .{ .truncate = true, .read = false });
+    defer file.close();
+
+    var dial_position: i64 = 50;
+    var dial_zero_count: u64 = 0;
 
     while (lines.next()) |line| {
         const direction: Direction = if (line[0] == 'R') .right else .left;
-        const turns: i64 = try std.fmt.parseInt(i64, line[1..], 0);
+        const start_dial_position = dial_position;
+        var turns = try std.fmt.parseInt(i64, line[1..], 0);
 
-        if (direction == .right) dialPosition += turns else dialPosition -= turns;
+        // Any count in excess of 100 does a full rotation
+        while (turns > 100) : (turns -= 100) {
+            dial_zero_count += 1;
+        }
 
-        while (dialPosition > 99) dialPosition -= 100;
-        while (dialPosition < 0) dialPosition += 100;
+        if (direction == .right) dial_position += turns else dial_position -= turns;
 
-        std.debug.print("{s} {d} => {d}", .{ @tagName(direction), turns, dialPosition });
-        if (dialPosition == 0) {
-            dialZeroCount += 1;
-            std.debug.print(" - added\n", .{});
-        } else {
-            std.debug.print("\n", .{});
+        // Case 1: We passed zero going right
+        if (dial_position > 99) {
+            dial_position -= 100;
+            dial_zero_count += 1;
+        }
+        // Case 2: We ended on zero
+        else if (dial_position == 0) {
+            dial_zero_count += 1;
+        }
+        // Case 3: We passed zero going left
+        else if (dial_position < 0) {
+            dial_position += 100;
+            // Only increment zero count if we were not already on zero to avoid double counting
+            if (start_dial_position > 0) dial_zero_count += 1;
         }
     }
 
-    std.debug.print("Zero count: {d}\n", .{dialZeroCount});
+    std.debug.print("Zero count: {d}\n", .{dial_zero_count});
+    try std.testing.expectEqual(6228, dial_zero_count);
 }
