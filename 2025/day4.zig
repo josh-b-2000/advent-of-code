0d199421bd3afb07905d86f0daf788ce696dba15
josh-b-2000 <joshua@autone.io>
2025-12-04 21:14:20 +0000

    day 4 part 1

---
+++ 2025/day4.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+const data = @embedFile("./day4_input.txt");
+
+const parseInt = std.fmt.parseInt;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) @panic("Uh oh");
+    }
+
+    var lines = std.mem.tokenizeSequence(u8, data, "\n");
+
+    var map = std.AutoArrayHashMap(usize, u8).init(allocator);
+    defer map.deinit();
+
+    const first_line = lines.peek().?;
+    const col_count = first_line.len;
+
+    var row_index: usize = 0;
+    while (lines.next()) |line| {
+        for (line, 0..) |char, col_index| {
+            if (char == '@') try map.put(row_index * col_count + col_index, 0);
+        }
+
+        row_index += 1;
+    }
+
+    var count_valid: usize = 0;
+    for (map.keys()) |test_index| {
+        const is_left = test_index % col_count == 0;
+        const is_right = (test_index + 1) % col_count == 0;
+
+        var touching_count: usize = 0;
+
+        // Can check vertical independent of top/bottom
+        // - Need to be wary of going negative though (integer overflow)
+        if (test_index > col_count and map.contains(test_index - col_count)) touching_count += 1;
+        if (map.contains(test_index + col_count)) touching_count += 1;
+
+        if (!is_left) {
+            if (test_index > col_count and map.contains(test_index - col_count - 1)) touching_count += 1;
+            if (map.contains(test_index - 1)) touching_count += 1;
+            if (map.contains(test_index + col_count - 1)) touching_count += 1;
+        }
+
+        if (!is_right) {
+            if (test_index > col_count and map.contains(test_index - col_count + 1)) touching_count += 1;
+            if (map.contains(test_index + 1)) touching_count += 1;
+            if (map.contains(test_index + col_count + 1)) touching_count += 1;
+        }
+
+        if (touching_count < 4) count_valid += 1;
+        std.debug.print("\n{d} - {d} ({d})\n", .{ test_index, touching_count, count_valid });
+    }
+
+    for (map.keys()) |val| std.debug.print("{d}\n", .{val});
+
+    std.debug.print("\n{d}\n", .{count_valid});
+    try std.testing.expectEqual(count_valid, 1457);
+}
