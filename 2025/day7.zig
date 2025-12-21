818a2b762df37b209d60f4cc74345b1db5c788c2
josh-b-2000 <joshua@autone.io>
2025-12-21 20:34:58 +0000

    day 7 part 2 (got solution from reddit sadly - too small brain)

--- 2025/day7.zig
+++ 2025/day7.zig
@@ -14,36 +14,46 @@ pub fn main() !void {
 
     const length = lines.peek().?.len;
 
-    var beams = try allocator.alloc(bool, length);
+    var beams = try allocator.alloc(usize, length);
     defer allocator.free(beams);
 
-    for (0..length) |i| beams[i] = false;
+    for (0..length) |i| beams[i] = 0;
 
-    var split_count: usize = 0;
+    // First line contains the starting beam position
+    const first_line = lines.next().?;
+    for (first_line, 0..) |char, index| {
+        if (char == 'S') {
+            beams[index] = 1;
+            break;
+        }
+    }
+
     while (lines.next()) |line| {
-        var curr = try allocator.alloc(bool, length);
+        var curr = try allocator.alloc(usize, length);
         defer allocator.free(curr);
 
         for (0..length) |i| curr[i] = beams[i];
 
         for (line, 0..) |char, index| {
             switch (char) {
-                'S' => beams[index] = true,
-                '^' => if (curr[index]) {
-                    if (index - 1 >= 0) beams[index - 1] = true;
-                    beams[index] = false;
-                    if (index + 1 < length) beams[index + 1] = true;
-                    split_count += 1;
+                '^' => if (curr[index] > 0) {
+                    if (index - 1 >= 0) beams[index - 1] += curr[index];
+                    if (index + 1 < length) beams[index + 1] += curr[index];
+
+                    beams[index] = 0;
                 },
                 else => {},
             }
         }
 
         std.debug.print("{s}\n", .{line});
-        for (beams) |b| std.debug.print("{s}", .{if (b) "|" else " "});
+        for (beams) |b| std.debug.print("{d}", .{b});
         std.debug.print("\n", .{});
     }
 
-    std.debug.print("{d}\n", .{split_count});
-    try std.testing.expectEqual(1516, split_count);
+    var total: usize = 0;
+    for (beams) |b| total += b;
+
+    std.debug.print("{d}\n", .{total});
+    try std.testing.expectEqual(1393669447690, total);
 }
